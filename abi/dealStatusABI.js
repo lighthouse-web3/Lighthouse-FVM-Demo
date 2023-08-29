62052f526038e318ffe8088e38e2ea975a2a7ef4
ravish <ravish1729@gmail.com>
2023-08-29 21:13:39 +0530

    first commit

---
+++ abi/dealStatusABI.js
@@ -0,0 +1,708 @@
+module.exports = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "int256",
+        "name": "errorCode",
+        "type": "int256"
+      }
+    ],
+    "name": "ActorError",
+    "type": "error"
+  },
+  {
+    "inputs": [],
+    "name": "FailToCallActor",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint64",
+        "name": "",
+        "type": "uint64"
+      }
+    ],
+    "name": "InvalidCodec",
+    "type": "error"
+  },
+  {
+    "inputs": [],
+    "name": "InvalidResponseLength",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "balance",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "NotEnoughBalance",
+    "type": "error"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "indexed": true,
+        "internalType": "uint64",
+        "name": "dealId",
+        "type": "uint64"
+      }
+    ],
+    "name": "CompleteAggregatorRequest",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "bytes",
+        "name": "cid",
+        "type": "bytes"
+      }
+    ],
+    "name": "SubmitAggregatorRequest",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_id",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint64",
+        "name": "_dealId",
+        "type": "uint64"
+      },
+      {
+        "internalType": "uint64",
+        "name": "_minerId",
+        "type": "uint64"
+      },
+      {
+        "components": [
+          {
+            "components": [
+              {
+                "internalType": "uint64",
+                "name": "index",
+                "type": "uint64"
+              },
+              {
+                "internalType": "bytes32[]",
+                "name": "path",
+                "type": "bytes32[]"
+              }
+            ],
+            "internalType": "struct ProofData",
+            "name": "proofSubtree",
+            "type": "tuple"
+          },
+          {
+            "components": [
+              {
+                "internalType": "uint64",
+                "name": "index",
+                "type": "uint64"
+              },
+              {
+                "internalType": "bytes32[]",
+                "name": "path",
+                "type": "bytes32[]"
+              }
+            ],
+            "internalType": "struct ProofData",
+            "name": "proofIndex",
+            "type": "tuple"
+          }
+        ],
+        "internalType": "struct InclusionProof",
+        "name": "_proof",
+        "type": "tuple"
+      },
+      {
+        "components": [
+          {
+            "internalType": "bytes",
+            "name": "commPc",
+            "type": "bytes"
+          },
+          {
+            "internalType": "uint64",
+            "name": "sizePc",
+            "type": "uint64"
+          }
+        ],
+        "internalType": "struct InclusionVerifierData",
+        "name": "_verifierData",
+        "type": "tuple"
+      }
+    ],
+    "name": "complete",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "bytes",
+            "name": "commPa",
+            "type": "bytes"
+          },
+          {
+            "internalType": "uint64",
+            "name": "sizePa",
+            "type": "uint64"
+          }
+        ],
+        "internalType": "struct InclusionAuxData",
+        "name": "",
+        "type": "tuple"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "components": [
+          {
+            "internalType": "bytes32",
+            "name": "commDs",
+            "type": "bytes32"
+          },
+          {
+            "internalType": "uint64",
+            "name": "offset",
+            "type": "uint64"
+          },
+          {
+            "internalType": "uint64",
+            "name": "size",
+            "type": "uint64"
+          },
+          {
+            "internalType": "bytes16",
+            "name": "checksum",
+            "type": "bytes16"
+          }
+        ],
+        "internalType": "struct SegmentDesc",
+        "name": "_sd",
+        "type": "tuple"
+      }
+    ],
+    "name": "computeChecksum",
+    "outputs": [
+      {
+        "internalType": "bytes16",
+        "name": "",
+        "type": "bytes16"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "components": [
+          {
+            "components": [
+              {
+                "internalType": "uint64",
+                "name": "index",
+                "type": "uint64"
+              },
+              {
+                "internalType": "bytes32[]",
+                "name": "path",
+                "type": "bytes32[]"
+              }
+            ],
+            "internalType": "struct ProofData",
+            "name": "proofSubtree",
+            "type": "tuple"
+          },
+          {
+            "components": [
+              {
+                "internalType": "uint64",
+                "name": "index",
+                "type": "uint64"
+              },
+              {
+                "internalType": "bytes32[]",
+                "name": "path",
+                "type": "bytes32[]"
+              }
+            ],
+            "internalType": "struct ProofData",
+            "name": "proofIndex",
+            "type": "tuple"
+          }
+        ],
+        "internalType": "struct InclusionProof",
+        "name": "ip",
+        "type": "tuple"
+      },
+      {
+        "components": [
+          {
+            "internalType": "bytes",
+            "name": "commPc",
+            "type": "bytes"
+          },
+          {
+            "internalType": "uint64",
+            "name": "sizePc",
+            "type": "uint64"
+          }
+        ],
+        "internalType": "struct InclusionVerifierData",
+        "name": "verifierData",
+        "type": "tuple"
+      }
+    ],
+    "name": "computeExpectedAuxData",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "bytes",
+            "name": "commPa",
+            "type": "bytes"
+          },
+          {
+            "internalType": "uint64",
+            "name": "sizePa",
+            "type": "uint64"
+          }
+        ],
+        "internalType": "struct InclusionAuxData",
+        "name": "",
+        "type": "tuple"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint64",
+        "name": "dealId",
+        "type": "uint64"
+      },
+      {
+        "components": [
+          {
+            "components": [
+              {
+                "internalType": "uint64",
+                "name": "index",
+                "type": "uint64"
+              },
+              {
+                "internalType": "bytes32[]",
+                "name": "path",
+                "type": "bytes32[]"
+              }
+            ],
+            "internalType": "struct ProofData",
+            "name": "proofSubtree",
+            "type": "tuple"
+          },
+          {
+            "components": [
+              {
+                "internalType": "uint64",
+                "name": "index",
+                "type": "uint64"
+              },
+              {
+                "internalType": "bytes32[]",
+                "name": "path",
+                "type": "bytes32[]"
+              }
+            ],
+            "internalType": "struct ProofData",
+            "name": "proofIndex",
+            "type": "tuple"
+          }
+        ],
+        "internalType": "struct InclusionProof",
+        "name": "ip",
+        "type": "tuple"
+      },
+      {
+        "components": [
+          {
+            "internalType": "bytes",
+            "name": "commPc",
+            "type": "bytes"
+          },
+          {
+            "internalType": "uint64",
+            "name": "sizePc",
+            "type": "uint64"
+          }
+        ],
+        "internalType": "struct InclusionVerifierData",
+        "name": "verifierData",
+        "type": "tuple"
+      }
+    ],
+    "name": "computeExpectedAuxDataWithDeal",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "bytes",
+            "name": "commPa",
+            "type": "bytes"
+          },
+          {
+            "internalType": "uint64",
+            "name": "sizePa",
+            "type": "uint64"
+          }
+        ],
+        "internalType": "struct InclusionAuxData",
+        "name": "",
+        "type": "tuple"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "left",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "right",
+        "type": "bytes32"
+      }
+    ],
+    "name": "computeNode",
+    "outputs": [
+      {
+        "internalType": "bytes32",
+        "name": "",
+        "type": "bytes32"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "components": [
+          {
+            "internalType": "uint64",
+            "name": "index",
+            "type": "uint64"
+          },
+          {
+            "internalType": "bytes32[]",
+            "name": "path",
+            "type": "bytes32[]"
+          }
+        ],
+        "internalType": "struct ProofData",
+        "name": "d",
+        "type": "tuple"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "subtree",
+        "type": "bytes32"
+      }
+    ],
+    "name": "computeRoot",
+    "outputs": [
+      {
+        "internalType": "bytes32",
+        "name": "",
+        "type": "bytes32"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes",
+        "name": "_cid",
+        "type": "bytes"
+      }
+    ],
+    "name": "getActiveDeals",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "uint64",
+            "name": "dealId",
+            "type": "uint64"
+          },
+          {
+            "internalType": "uint64",
+            "name": "minerId",
+            "type": "uint64"
+          }
+        ],
+        "internalType": "struct IAggregatorOracle.Deal[]",
+        "name": "",
+        "type": "tuple[]"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes",
+        "name": "_cid",
+        "type": "bytes"
+      }
+    ],
+    "name": "getAllDeals",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "uint64",
+            "name": "dealId",
+            "type": "uint64"
+          },
+          {
+            "internalType": "uint64",
+            "name": "minerId",
+            "type": "uint64"
+          }
+        ],
+        "internalType": "struct IAggregatorOracle.Deal[]",
+        "name": "",
+        "type": "tuple[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes",
+        "name": "_cid",
+        "type": "bytes"
+      },
+      {
+        "internalType": "uint64",
+        "name": "epochs",
+        "type": "uint64"
+      }
+    ],
+    "name": "getExpiringDeals",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "uint64",
+            "name": "dealId",
+            "type": "uint64"
+          },
+          {
+            "internalType": "uint64",
+            "name": "minerId",
+            "type": "uint64"
+          }
+        ],
+        "internalType": "struct IAggregatorOracle.Deal[]",
+        "name": "",
+        "type": "tuple[]"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "left",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "right",
+        "type": "bytes32"
+      }
+    ],
+    "name": "hashNode",
+    "outputs": [
+      {
+        "internalType": "bytes32",
+        "name": "",
+        "type": "bytes32"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "components": [
+          {
+            "internalType": "bytes32",
+            "name": "commDs",
+            "type": "bytes32"
+          },
+          {
+            "internalType": "uint64",
+            "name": "offset",
+            "type": "uint64"
+          },
+          {
+            "internalType": "uint64",
+            "name": "size",
+            "type": "uint64"
+          },
+          {
+            "internalType": "bytes16",
+            "name": "checksum",
+            "type": "bytes16"
+          }
+        ],
+        "internalType": "struct SegmentDesc",
+        "name": "sd",
+        "type": "tuple"
+      }
+    ],
+    "name": "serialize",
+    "outputs": [
+      {
+        "internalType": "bytes",
+        "name": "",
+        "type": "bytes"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes",
+        "name": "_cid",
+        "type": "bytes"
+      }
+    ],
+    "name": "submit",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes",
+        "name": "data",
+        "type": "bytes"
+      }
+    ],
+    "name": "truncatedHash",
+    "outputs": [
+      {
+        "internalType": "bytes32",
+        "name": "",
+        "type": "bytes32"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "components": [
+          {
+            "internalType": "uint64",
+            "name": "index",
+            "type": "uint64"
+          },
+          {
+            "internalType": "bytes32[]",
+            "name": "path",
+            "type": "bytes32[]"
+          }
+        ],
+        "internalType": "struct ProofData",
+        "name": "proof",
+        "type": "tuple"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "root",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "leaf",
+        "type": "bytes32"
+      }
+    ],
+    "name": "verify",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  }
+]
--- scripts/submit.js
+++ scripts/submit.js
@@ -1,57 +1,28 @@
 require("dotenv").config();
 const { ethers } = require("ethers");
-const aggregatorAbi = require("../abi/aggregatorABI");
+const dealStatusABI = require("../abi/dealStatusABI");
 
 const submit = async () => {
-  try {
-    console.log("Transaction execution started...");
-    const provider = new ethers.providers.JsonRpcProvider(
-      "https://api.calibration.node.glif.io/rpc/v1"
-    );
-    const privateKey = process.env.PRIVATE_KEY; //wallet private key
-    const signer = new ethers.Wallet(privateKey, provider);
+  const provider = new ethers.providers.JsonRpcProvider("https://api.calibration.node.glif.io/rpc/v1");
+  const privateKey = process.env.PRIVATE_KEY;
+  const signer = new ethers.Wallet(privateKey, provider);
 
-    const contractAddress = "0x201c21E051D25E4F5F8B3e40Fb019cdeB26301aC";
+  const contractAddress = "0x6ec8722e6543fB5976a547434c8644b51e24785b"
 
-    const contract = new ethers.Contract(
-      contractAddress,
-      aggregatorAbi,
-      signer
-    );
+  const contract = new ethers.Contract(contractAddress, dealStatusABI, signer);
 
-    // Follow this documentation (https://docs.lighthouse.storage/lighthouse-1/lighthouse-sdk/code-examples/nodejs-backend/nodejs) to upload file via Lighthouse SDK
-    // Transaction details
-    const data_url =
-      "https://gateway.lighthouse.storage/ipfs/QmQvQwY42QuukKKWB52qtxMQHVNFM6kjz4Pq3sQwN5f7xY";
-    const fileLinkBytes = ethers.utils.hexlify(
-      ethers.utils.toUtf8Bytes(data_url)
-    );
+  const cid = "Qmc81EUDDKH75ZA1D1cD8UZ6DqNvEgcwhmC8mWGid7YnYy";
+  const cidHex = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(cid));
+  const submit = await contract.submit(cidHex, {
+    gasLimit: 50_000_000,
+  });
 
-    // order- file_url
-    const submit = await contract.submit(fileLinkBytes, {
-      gasLimit: 50_000_000,
-    });
+  const response = await submit.wait();
+  console.log("transaction done");
 
-    const response = await submit.wait();
-    console.log("transaction done");
-
-    const eventData = response.events[0].args;
-    // Assuming there is only one event in the response, you can access it like this:
-
-    // Assuming the event has three parameters: bytes, uint256, and address
-    const uint256Data = eventData[0];
-    const fileLinkHex = eventData[1];
-    const fileLinkByte = ethers.utils.arrayify(fileLinkHex);
-    const addressData = eventData[2];
-
-    // Now you can work with the event data as needed
-    console.log("File Link", ethers.utils.toUtf8String(fileLinkByte));
-    console.log("Transaction Id:", uint256Data);
-    console.log("Transaction Id in Number:", Number(uint256Data));
-    console.log("User Address:", addressData);
-  } catch (error) {
-    console.log(error.message);
-  }
+  const eventData = response.events[0].args;
+  console.log('Tx Id:', Number(eventData[0]));
+  console.log('CID', ethers.utils.toUtf8String(eventData[1]));
 };
 
-submit();
+submit()
