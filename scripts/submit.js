9b200f496c90871fbc10a6240133d75382f86c58
ravish <ravish1729@gmail.com>
2023-09-01 17:34:08 +0530

    comments

--- scripts/submit.js
+++ scripts/submit.js
@@ -1,28 +1,28 @@
-require("dotenv").config();
-const { ethers } = require("ethers");
-const dealStatusABI = require("../abi/dealStatusABI");
+require("dotenv").config()
+import { ethers } from "ethers"
+import dealStatusABI from "../abi/dealStatusABI"
 
 const submit = async () => {
-  const provider = new ethers.providers.JsonRpcProvider("https://api.calibration.node.glif.io/rpc/v1");
-  const privateKey = process.env.PRIVATE_KEY;
-  const signer = new ethers.Wallet(privateKey, provider);
+  // Signer
+  const provider = new ethers.providers.JsonRpcProvider("https://api.calibration.node.glif.io/rpc/v1")
+  const privateKey = process.env.PRIVATE_KEY
+  const signer = new ethers.Wallet(privateKey, provider)
 
+  // Contract
   const contractAddress = "0x6ec8722e6543fB5976a547434c8644b51e24785b"
+  const contract = new ethers.Contract(contractAddress, dealStatusABI, signer)
 
-  const contract = new ethers.Contract(contractAddress, dealStatusABI, signer);
-
-  const cid = "Qmc81EUDDKH75ZA1D1cD8UZ6DqNvEgcwhmC8mWGid7YnYy";
-  const cidHex = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(cid));
+  // Call submit
+  const cid = "QmPb9ghtNywrmK3KbqnQFNJmijNk1sbjhZa37LZg4wgPKy"
+  const cidHex = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(cid))
   const submit = await contract.submit(cidHex, {
     gasLimit: 50_000_000,
-  });
+  })
 
-  const response = await submit.wait();
-  console.log("transaction done");
-
-  const eventData = response.events[0].args;
-  console.log('Tx Id:', Number(eventData[0]));
-  console.log('CID', ethers.utils.toUtf8String(eventData[1]));
+  const response = await submit.wait()
+  const eventData = response.events[0].args
+  console.log('Tx Id:', Number(eventData[0]))
+  console.log('CID', ethers.utils.toUtf8String(eventData[1]))
 };
 
 submit()
